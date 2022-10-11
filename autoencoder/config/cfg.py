056d7306709ea8f8f926838c3ff413a4174142bc
william.ramirez <william.lnx@gmail.com>
2022-10-11 09:47:29 -0500

    Configuration added

---
+++ autoencoder/config/cfg.py
@@ -0,0 +1,12 @@
+'''
+Created by: William Ramírez
+Email: william.ramirez@spotcloud.io
+
+'''
+
+
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
+IMG_TRAIN_PATH = "data/datasets/train"
+IMG_VAL_PATH = "data/datasets/val"
+IMG_TEST_PATH = "data/datasets/test"
