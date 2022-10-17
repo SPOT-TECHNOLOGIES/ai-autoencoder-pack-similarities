3db02b7e19e9cf0766b10e005ab75d9516009c86
william.ramirez <william.lnx@gmail.com>
2022-10-17 09:14:09 -0500

    Indexes of pallet returning in the embedding

--- autoencoder/utils/create_embedding.py
+++ autoencoder/utils/create_embedding.py
@@ -3,16 +3,19 @@ Created by: William Ramírez
 Email: william.ramirez@spotcloud.io
 
 '''
-
+import torch
+import numpy as np
 
 def create_embedding(encoder, full_loader, embedding_dim, device):
     encoder.eval()
     embedding = torch.randn(embedding_dim)
+    indexes = []
 
     with torch.no_grad():
-        for batch_idx, (train_img, target_img) in enumerate(full_loader):
-            train_img = train_img.to(device)
-            enc_output = encoder(train_img).cpu()
+        for batch_idx, (img, img_id) in enumerate(full_loader):
+            img = img.to(device)
+            enc_output = encoder(img).cpu()
             embedding = torch.cat((embedding, enc_output), 0)
-    
-    return embedding
+            indexes.append(img_id.tolist())
+            
+    return embedding.cpu().detach().numpy()[1::,:],np.array(indexes).flatten()
