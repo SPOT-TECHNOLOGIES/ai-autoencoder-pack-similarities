1383096787570053a90e1a4e008cd20d56fdbb4a
william.ramirez <william.lnx@gmail.com>
2022-10-17 09:11:58 -0500

    Similarity for a collection of images implemented

--- sim_api.py
+++ sim_api.py
@@ -19,57 +19,62 @@ import cv2
 
 import torch
 from autoencoder.utils import *
+from autoencoder.data.custom_dataset import jsonDataset
+from autoencoder.config import *
 
 app = Flask(__name__)
 
 @app.route("/similarity",methods=['POST'])
 
-# http://ec2-52-22-86-23.compute-1.amazonaws.com:443/compare
 
 def compare():
 
-	#reading images
 	json_request = request.get_json()
 
-	img_req2 = json_request["pallet_a"]
-	img_req1 = json_request["pallet_b"]
+	collection = jsonDataset(json_request)
+	query_img_id = json_request["data"]["query"][0]["pallet_id"]
+	query_img_b64 = json_request["data"]["query"][0]["base64"]
 
-	img_bytes1 = base64.b64decode(img_req1)
-	img_bytes2 = base64.b64decode(img_req2)
-
-	im_arr1 = np.frombuffer(img_bytes1,dtype = np.uint8)
-	im_arr2 = np.frombuffer(img_bytes2,dtype = np.uint8)
-
-	img_dec1 = cv2.imdecode(im_arr1, \
+	img_bytes = base64.b64decode(query_img_b64)
+	im_arr = np.frombuffer(img_bytes,dtype = np.uint8)
+	img_dec = cv2.imdecode(im_arr, \
 		flags=cv2.IMREAD_COLOR)[:, :, ::-1]
-	img_dec2 = cv2.imdecode(im_arr2, \
-		flags=cv2.IMREAD_COLOR)[:, :, ::-1]
-
-	img_dec_res1 = cv2.resize(img_dec1, \
+	img_dec_res = cv2.resize(img_dec, \
 		tuple([IMG_WIDTH,IMG_HEIGHT]), \
 		interpolation=cv2.INTER_CUBIC)
-	img_dec_res2 = cv2.resize(img_dec2, \
-		tuple([IMG_WIDTH,IMG_HEIGHT]), \
-		interpolation=cv2.INTER_CUBIC)
-
-	img_t1 =  torch.as_tensor(img_dec_res1.astype("float32") \
-		.transpose(2, 0, 1))
-	img_t2 =  torch.as_tensor(img_dec_res2.astype("float32") \
+	query_tensor =  torch.as_tensor(img_dec_res.astype("float32") \
 		.transpose(2, 0, 1))
 
-	encoder = create_encoder("cpu")
-	f1 = extract_image_feature(img_t1,encoder,"cpu")
-	f2 = extract_image_feature(img_t2,encoder,"cpu")
+	collection_loader = torch.utils.data.DataLoader(
+		collection,batch_size=collection.ncollect)
 
-	sim = np.matmul(f1,f2.transpose())[0][0]
+	if torch.cuda.is_available():
+		device = "cuda"
+	else:
+		device = "cpu"
 
-	return str(sim)
+	encoder = create_encoder(device)
+
+	embedding, pallet_ids = create_embedding(encoder, collection_loader,EMBEDDING_DIM, device)
+	embedding = embedding.reshape((embedding.shape[0],-1))
+
+	k_idx,query_feat = compute_similar_images(query_tensor,2,\
+		embedding,encoder,device)
+	query_feat_n = query_feat/np.linalg.norm(query_feat)
+	emb_n = np.array([embedding[i,:]/np.linalg.norm(embedding[i,:]) for i in k_idx[0]])
+	sim = np.matmul(query_feat_n,emb_n.transpose())
+
+	res = jsonify({"data":{"similars":[{"pallet_id":int(pallet_ids[k_idx[0][0]]) \
+		,"similarity":float(sim[0][0])},{"pallet_id":int(pallet_ids[k_idx[0][1]]) \
+		,"similarity":float(sim[0][1])}]}})
+
+	return res
 
 
 if __name__== "__main__":
-	# app.run(debug= False, port= 6006)
+	app.run(debug= False, port= 6006)
 	# app.run(debug= False, port= 443,host='0.0.0.0')
-	app.run(debug= True,host='0.0.0.0', port=8082)
+	# app.run(debug= True,host='0.0.0.0', port=8082)
 
 
 
